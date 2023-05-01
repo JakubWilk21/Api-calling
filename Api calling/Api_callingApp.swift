deacabebeb6d5956809df2cf33eeb4aa9ee2502d
Jakub <Wilk>
2023-04-30 22:09:49 -0500

    Initial Commit

---
+++ Api calling/Api_callingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Api_callingApp.swift
+//  Api calling
+//
+//  Created by Jakub Wilk on 4/30/23.
+//
+
+import SwiftUI
+
+@main
+struct Api_callingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
