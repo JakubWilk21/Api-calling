6d656b5fdc54fe1d1a72b858900db3d9cc494a57
Jakub <Wilk>
2023-05-10 10:03:01 -0500

    fixed content view

--- Api calling/ContentView.swift
+++ Api calling/ContentView.swift
@@ -3,24 +3,62 @@
 //  Api calling
 //
 //  Created by Jakub Wilk on 4/30/23.
-//
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var facts = [String]()
+    @State private var showingAlert = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List(facts, id: \.self) { fact in
+                Text(fact)
+            }
+            .navigationTitle("Random Cat Facts")
+            .toolbar {
+                Button(action: {
+                    Task {
+                        await loadData()
+                    }
+                }, label: {
+                    Image(systemName: "arrow.clockwise")
+                })
+                .alert(isPresented: $showingAlert) {
+                    Alert(title: Text("Loading Error"),
+                          message: Text("There was a problem loading the API categories"),
+                          dismissButton: .default(Text("OK")))
+                }
+            }
         }
-        .padding()
+        .task {
+            await loadData()
+        }
+    }
+    
+    
+    func loadData() async {
+        if let url = URL(string: "https://meowfacts.herokuapp.com/?count=20")  {
+            if let (data, _) = try? await URLSession.shared.data(from: url) {
+                if let decodedResponse = try? JSONDecoder().decode(Facts.self, from: data) {
+                    facts = decodedResponse.facts
+                }
+            }
+        }
+        showingAlert = true
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
-}
+    
+    struct Facts: Identifiable, Codable {
+        var id = UUID()
+        var facts: [String]
+        
+        enum CodingKeys: String, CodingKey {
+            case facts = "data"
+        }
+    }
